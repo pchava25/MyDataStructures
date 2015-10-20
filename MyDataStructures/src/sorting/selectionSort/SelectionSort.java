5cf7e8eb0d96d935f2dc6362613f5b3f8a3f9372
pchava25 <pchava25@gmail.com>
2015-10-20 14:57:05 -0500

    changes

--- MyDataStructures/src/sorting/selectionSort/SelectionSort.java
+++ MyDataStructures/src/sorting/selectionSort/SelectionSort.java
@@ -1,6 +1,5 @@
 package sorting.selectionSort;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
@@ -13,27 +12,18 @@ public class SelectionSort<E extends Comparable<E>>  {
 		int min=0;
 		for(int i=0;i<sortArray.size();i++)
 		{
+			min=i;
 			for(int j=i+1;j<sortArray.size();j++)
-			{
-				System.out.println(sortArray.get(j).compareTo(sortArray.get(min)));
+			{				
 				if(sortArray.get(j).compareTo(sortArray.get(min))<0)
-					{						
-					System.out.println("hello 1");
+					{
 					min=j;
 					}
 			}
 			if(min!=i){
-				System.out.println("hello 2");
-				swap(sortArray.get(i),sortArray.get(min));
-				System.out.println(sortArray.get(i)+" "+sortArray.get(min));
 				swap(sortArray,i,min);
-				System.out.println(sortArray.get(i)+" "+sortArray.get(min));
 			}
 			
-		}
-		for(int i=0;i<sortArray.size();i++)
-		{
-			System.out.print(sortArray.get(i)+" ");
 		}
 		return sortArray;
 	}
@@ -44,7 +34,6 @@ public class SelectionSort<E extends Comparable<E>>  {
 		temp=a;
 		a=b;
 		b=temp;
-		System.out.println(a+" "+b);
 	}
 	public <E> void swap(List<E> a, int i, int j) {
         E temp = a.get(i);
